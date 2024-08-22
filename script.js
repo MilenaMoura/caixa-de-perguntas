eb4b2a84737db0bb9e6418d5b77f681033809b6e
Milena Moura <105373502+MilenaMoura@users.noreply.github.com>
2024-08-22 08:06:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -53,20 +53,20 @@ const perguntas = [
             },
             {
                 texto: "Não.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                afirmacao: "Gosta mais de aulas teóricas"
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Você faria curso regular ou intensivo?",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Regular. Quero aprender o idioma com calma contendo menos aulas na semana",
+                afirmacao: "Faz o curso com mais tempo."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Intensivo. Quero aprender o idioma em curto prazo, contendo mais aulas durante a semana",
+                afirmacao: "o aluno tem pressa para terminar o curso e prefere fazer mais aulas."
             }
         ]
     },
