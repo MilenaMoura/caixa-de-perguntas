82d535afb4a2b16848e49206e62b2cfd944f6602
Milena Moura <105373502+MilenaMoura@users.noreply.github.com>
2024-08-22 08:11:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "O aluno...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
